a127884f23530c45220eea608699658bc89809c6
The EJ Factor <evan2s.jr@icloud.com>
2024-11-05 16:37:06 -0500

    Initial commit

---
+++ src/main/java/net/theejfactor/rainworldmcaitest/RainworldMCAITestClient.java
@@ -0,0 +1,11 @@
+package net.theejfactor.rainworldmcaitest;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RainworldMCAITestClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
+
